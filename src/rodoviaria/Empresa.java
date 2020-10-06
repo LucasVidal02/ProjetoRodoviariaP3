8618670d840956b9fed8e041af1433a214412dfc
Lucas Arruda Vidal <lucas.2019107665@unicap.br>
2020-10-06 16:33:52 -0300

    Update Empresa.java

--- src/rodoviaria/Empresa.java
+++ src/rodoviaria/Empresa.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rodoviaria;
 
-/**
- *
- * @author LucasArrudaVidal
- */
+
 public class Empresa {
+    
+    private String nome;
+    private Rotas rotas;
+    private Onibus onibus;
+    
     
 }
